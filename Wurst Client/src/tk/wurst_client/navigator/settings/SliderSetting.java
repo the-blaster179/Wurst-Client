454d212f30a325fb36a9934332897b295c292731
Alexander01998 <contact.wurstimperium@gmail.com>
2016-01-06 13:15:34 +0100

    Create SliderSetting class

---
+++ Wurst Client/src/tk/wurst_client/navigator/settings/SliderSetting.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2014 - 2016 | Alexander01998 and contributors
+ * All rights reserved.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package tk.wurst_client.navigator.settings;
+
+import org.darkstorm.minecraft.gui.component.basic.BasicSlider;
+
+public class SliderSetting extends BasicSlider implements NavigatorSetting
+{	
+	
+}
