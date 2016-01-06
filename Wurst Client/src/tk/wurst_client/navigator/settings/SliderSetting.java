c24f52d9e789f225f42cfb9b0402995899d98289
Alexander01998 <contact.wurstimperium@gmail.com>
2016-01-06 13:20:28 +0100

    Add constructors to SliderSetting

--- Wurst Client/src/tk/wurst_client/navigator/settings/SliderSetting.java
+++ Wurst Client/src/tk/wurst_client/navigator/settings/SliderSetting.java
@@ -11,6 +11,37 @@ package tk.wurst_client.navigator.settings;
 import org.darkstorm.minecraft.gui.component.basic.BasicSlider;
 
 public class SliderSetting extends BasicSlider implements NavigatorSetting
-{	
+{
+	public SliderSetting()
+	{
+		super();
+	}
 	
+	public SliderSetting(String text, double value, double minimum,
+		double maximum, double increment, ValueDisplay display)
+	{
+		super(text, value, minimum, maximum, increment, display);
+	}
+	
+	public SliderSetting(String text, double value, double minimum,
+		double maximum, int increment)
+	{
+		super(text, value, minimum, maximum, increment);
+	}
+	
+	public SliderSetting(String text, double value, double minimum,
+		double maximum)
+	{
+		super(text, value, minimum, maximum);
+	}
+	
+	public SliderSetting(String text, double value)
+	{
+		super(text, value);
+	}
+	
+	public SliderSetting(String text)
+	{
+		super(text);
+	}
 }
