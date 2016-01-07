a2f319a275fbb2ddc80e3ecabcf4a7d84d0a5e35
ThisTestUser <testsubjectforka1@gmail.com>
2016-01-07 15:54:24 -0500

    Very small typo

--- Wurst Client/src/tk/wurst_client/mods/JesusMod.java
+++ Wurst Client/src/tk/wurst_client/mods/JesusMod.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 - 2015 Alexander01998 and contributors
+ * Copyright Â© 2014 - 2015 Alexander01998 and contributors
  * All rights reserved.
  * 
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -11,7 +11,7 @@ package tk.wurst_client.mods;
 @Mod.Info(category = Mod.Category.MOVEMENT,
 	description = "Allows you to walk on water.\n"
 		+ "The real Jesus used this hack ~2000 years ago.\n"
-		+ "Bypasses NoCheat+ is YesCheat+ is enabled.",
+		+ "Bypasses NoCheat+ if YesCheat+ is enabled.",
 	name = "Jesus")
 public class JesusMod extends Mod
 {
