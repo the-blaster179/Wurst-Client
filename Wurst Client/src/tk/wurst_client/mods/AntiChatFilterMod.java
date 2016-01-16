d37775d745fc3647b3c47b37a40888531ed7a663
kim-yannick <kim-yannick@users.noreply.github.com>
2016-01-16 14:42:58 +0100

    Created the AntiChatFilter mod

---
+++ Wurst Client/src/tk/wurst_client/mods/AntiChatFilterMod.java
@@ -0,0 +1,48 @@
+package tk.wurst_client.mods;
+
+import tk.wurst_client.WurstClient;
+import tk.wurst_client.events.ChatOutputEvent;
+import tk.wurst_client.events.listeners.ChatOutputListener;
+import tk.wurst_client.mods.Mod.Category;
+import tk.wurst_client.mods.Mod.Info;
+
+@Info(category = Category.CHAT,
+	description = "Uses a special font to bypass some\n"
+		+ "Word and Capslock filters.",
+	name = "AntiChatFilter")
+public class AntiChatFilterMod extends Mod implements ChatOutputListener
+{
+	private final String blacklist = "({[|]})";
+	
+	@Override
+	public void onEnable()
+	{
+		WurstClient.INSTANCE.events.add(ChatOutputListener.class, this);
+	}
+	
+	@Override
+	public void onSentMessage(ChatOutputEvent event)
+	{
+		if(event.getMessage().startsWith("/"))
+			return;
+			
+		String out = "";
+		
+		for(char chr : event.getMessage().toCharArray())
+		{
+			if(chr >= 0x21 && chr <= 0x80
+				&& !blacklist.contains(Character.toString(chr)))
+				out += new String(Character.toChars(((int)chr) + 0xFEE0));
+			else
+				out += chr;
+		}
+		
+		event.setMessage(out);
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		WurstClient.INSTANCE.events.remove(ChatOutputListener.class, this);
+	}
+}
--- Wurst Client/src/tk/wurst_client/mods/ModManager.java
+++ Wurst Client/src/tk/wurst_client/mods/ModManager.java
@@ -28,6 +28,7 @@ public class ModManager
 	public final AntiAfkMod antiAfkMod = new AntiAfkMod();
 	public final AntiBlindMod antiBlindMod = new AntiBlindMod();
 	public final AntiCactusMod antiCactusMod = new AntiCactusMod();
+	public final AntiChatFilterMod antiChatFilterMod = new AntiChatFilterMod();
 	public final AntiFireMod antiFireMod = new AntiFireMod();
 	public final AntiKnockbackMod antiKnockbackMod = new AntiKnockbackMod();
 	public final AntiMacMod antiMacMod = new AntiMacMod();
