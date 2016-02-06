c72d3023829f5822a4ef6d70e59ee082ff1c8f31
Alexander01998 <contact.wurstimperium@gmail.com>
2016-02-06 11:37:34 +0100

    Make other Killauras disable TP-Aura when they're enabled

--- Wurst Client/src/tk/wurst_client/mods/MultiAuraMod.java
+++ Wurst Client/src/tk/wurst_client/mods/MultiAuraMod.java
@@ -42,6 +42,8 @@ public class MultiAuraMod extends Mod implements UpdateListener
 			wurst.mods.killauraLegitMod.setEnabled(false);
 		if(wurst.mods.clickAuraMod.isEnabled())
 			wurst.mods.clickAuraMod.setEnabled(false);
+		if(wurst.mods.tpAuraMod.isEnabled())
+			wurst.mods.tpAuraMod.setEnabled(false);
 		if(wurst.mods.triggerBotMod.isEnabled())
 			wurst.mods.triggerBotMod.setEnabled(false);
 		wurst.events.add(UpdateListener.class, this);
